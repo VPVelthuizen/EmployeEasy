b1f2d44551b6feb3b80912bae705535b88b15a65
VPVelthuizen <vpvelthuizen@gmail.com>
2024-07-24 16:05:43 -0400

    minor tweaks

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL REFERENCES department(id)
+    department_id INTEGER NOT NULL REFERENCES departments(id)
 );
 
 -- Create the employee table
@@ -17,6 +17,6 @@ CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL REFERENCES role(id),
-    manager_id INTEGER REFERENCES employee(id)
+    role_id INTEGER NOT NULL REFERENCES roles(id),
+    manager_id INTEGER REFERENCES employees(id)
 );
